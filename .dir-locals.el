04b8eba20853a9b494c028e8ccb0642245c5c862
Zhang Qigang <zhangqigang@le.com>
2020-01-07 10:08:33 +0800

    caption changed.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,6 @@
-(("oxhugo" . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+("oxhugo"
+ (org-mode
+  (eval org-hugo-auto-export-mode)
+  (org-hugo-link-desc-insert-type . t)))
