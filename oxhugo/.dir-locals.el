0a1d24edd80543ff5b502b537bd441e24cf1fe5a
Zhang Qigang <zhangqigang@le.com>
2020-01-16 18:55:18 +0800

    update .dir-locals.

---
+++ oxhugo/.dir-locals.el
@@ -0,0 +1,4 @@
+ ((org-mode . ((eval . (org-hugo-auto-export-mode))
+     (org-hugo-link-desc-insert-type . t))))
+;;(("oxhugo". ((org-mode . ((eval . (org-hugo-auto-export-mode))
+;;                          (org-hugo-link-desc-insert-type . t))))))
